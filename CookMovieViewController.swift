5f2cd0b22e8a8f4a43d8aa2a0a2fd55ff2cf9bb2
wusu chiang <wusuchen@wusudeiMac.local>
2016-01-11 00:06:24 +0800

    ready

--- CookMovieViewController.swift
+++ CookMovieViewController.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import SafariServices
+import WebKit
 
-class CookMovieViewController: UIViewController {
+class CookMovieViewController: UIViewController, WKNavigationDelegate {
 
+    @IBOutlet weak var viewInWeb: UIView!
+    
     @IBAction func backtoPreView(sender: AnyObject) {
-        
         
         
         self.dismissViewControllerAnimated(true, completion: nil)
@@ -20,12 +23,28 @@ class CookMovieViewController: UIViewController {
 //        self.showViewController(controller!, sender: nil)
 //        self.presentViewController(controller!, animated: true, completion: nil)
         
-        
+    
     }
     override func viewDidLoad() {
-        super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
+//        let url = NSURL(string: "https://www.youtube.com/results?search_query=%E5%9C%B0%E7%93%9C+%E6%96%99%E7%90%86")
+//        _ = SFSafariViewController(URL: url!)
+//        self.dismissViewControllerAnimated(true, completion: nil)
+//        self.presentViewController(controller, animated:true, completion: nil)
+        
+        
+        
+        
+        
+        
+        let webView = WKWebView(frame:self.viewInWeb.frame)
+        webView.navigationDelegate = self
+        self.view.addSubview(webView)
+        let url = NSURL(string: "https://www.youtube.com/results?search_query=%E5%9C%B0%E7%93%9C+%E6%96%99%E7%90%86")
+        let request = NSURLRequest(URL: url!)
+        webView.loadRequest(request)
+
     }
 
     override func didReceiveMemoryWarning() {
